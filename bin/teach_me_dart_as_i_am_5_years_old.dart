7c68682c8868329a12ec366f70af0767c79019bf
Sanjib <sanjib12sinha@gmail.com>
2020-12-20 09:44:01 +0530

    first commit

---
+++ bin/teach_me_dart_as_i_am_5_years_old.dart
@@ -0,0 +1,5 @@
+import 'package:teach_me_dart_as_i_am_5_years_old/teach_me_dart_as_i_am_5_years_old.dart' as teach_me_dart_as_i_am_5_years_old;
+
+void main(List<String> arguments) {
+  print('Hello world: ${teach_me_dart_as_i_am_5_years_old.calculate()}!');
+}
---
+++ test/teach_me_dart_as_i_am_5_years_old_test.dart
@@ -0,0 +1,8 @@
+import 'package:teach_me_dart_as_i_am_5_years_old/teach_me_dart_as_i_am_5_years_old.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
