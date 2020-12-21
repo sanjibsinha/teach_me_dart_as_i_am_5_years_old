26170da5c34798f573f9a269f59a5d2f2764ea2a
Sanjib <sanjib12sinha@gmail.com>
2020-12-21 12:54:45 +0530

    sixth commit

--- bin/a_simple_class.dart
+++ bin/a_simple_class.dart
@@ -17,32 +17,13 @@ class ASimpleClass {
   }
 }
 
-/**
- * import 'package:teach_me_dart_as_i_am_5_years_old/a_simple_class.dart';
-import 'package:teach_me_dart_as_i_am_5_years_old/another_getter_setter.dart';
-import 'package:teach_me_dart_as_i_am_5_years_old/bear_class.dart';
-
 void main(List<String> arguments) {
   var firstInstance = ASimpleClass();
   firstInstance.firstName = 'John';
 
   firstInstance.getSecondName = 'Smith';
 
-  var bear = Bear();
-  bear.collarID = 1;
-  bear.setColor('Red');
-  bear.getColor;
-
-  var anInstance = AnotherGetterSetter();
-
-  anInstance.getName = 'Sanjib';
-
   print(
       '${firstInstance.firstName} ${firstInstance.getSecondName}'); // John Smith
-
-  print('${bear.collarID} ${bear.getColor}'); // 1 Red
-
-  print('${anInstance.getName}');
+  print('${firstInstance.getName()}'); // John Smith
 }
-
- */
--- bin/teach_me_dart_as_i_am_5_years_old.dart
+++ bin/teach_me_dart_as_i_am_5_years_old.dart
@@ -1,12 +1,7 @@
-import 'package:teach_me_dart_as_i_am_5_years_old/a_simple_class.dart';
 import 'package:teach_me_dart_as_i_am_5_years_old/another_getter_setter.dart';
 import 'package:teach_me_dart_as_i_am_5_years_old/bear_class.dart';
 
 void main(List<String> arguments) {
-  var firstInstance = ASimpleClass();
-  firstInstance.firstName = 'John';
-  firstInstance.getSecondName = 'Smith';
-
   var bear = Bear();
   bear.collarID = 1;
   bear.setColor('Red');
@@ -15,10 +10,6 @@ void main(List<String> arguments) {
   var anInstance = AnotherGetterSetter();
 
   anInstance.getName = 'Sanjib';
-
-  print(
-      '${firstInstance.firstName} ${firstInstance.getSecondName}'); // John Smith
-  print('${firstInstance.getName()}'); // John Smith
 
   print('${bear.collarID} ${bear.getColor}'); // 1 Red
 
